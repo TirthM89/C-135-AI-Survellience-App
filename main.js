e2c1ae9c7483c3c70f8dd1a152cd976aeeb06509
TirthM89 <98802520+TirthM89@users.noreply.github.com>
2023-03-16 17:23:22 +1030

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 video = "";
 status = "";
+objects = [];
 
 function preload() {
     video = createVideo("video.mp4");
@@ -7,13 +8,35 @@ function preload() {
 }
 
 function setup() {
-    canvas = createCanvas(480,380);
-    canvas.position(438,80);
+    canvas = createCanvas(300,250);
+    canvas.position(528,160);
 }
 
 function draw() {
-    image(video,0,0,480,380);
+    image(video,0,0,300,250);
+    if (status != "") {
+        objectDetector.detect(video,gotResult);
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status: objects detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of Objects: "+objects.length;
+            fill("#FF0000");
+            percent = Math.floor(objects[i].confidence*100);
+            text(objects[i].label+" "+percent+"%",objects[i].x+15,objects[i].y+15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        }
+    }
 }
+
+function gotResult(error,results) {
+    if (error) {
+        console.error(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
 
 function start() {
     objectDetector = ml5.objectDetector("cocossd", modelLoaded);
